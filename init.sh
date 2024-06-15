d6f3038d34c38853323577795164fac083b105bc
YuCheng1122 <tommymask00@gmail.com>
2024-06-15 19:38:08 +0800

    'rm_files'

--- init.sh
+++ init.sh
@@ -9,20 +9,11 @@ if [ $BASE_NAME != "HyperVision" ] && [ $BASE_NAME != "hypervision" ]; then
     exit -1
 fi
 
-# Download dataset.
-wget https://hypervision-publish.s3.cn-north-1.amazonaws.com.cn/hypervision-dataset.tar.gz
-tar -xxf hypervision-dataset.tar.gz
-rm $_
-
 # Build and run HyperVision.
 ./script/rebuild.sh
 ./script/expand.sh
 cd build && ../script/run_all_brute.sh && cd ..
 
-# Analyze the results.
-cd ./result_analyze
-./batch_analyzer.py -g brute
-cat ./log/brute/*.log | grep AU_ROC
-cd -
+# ./HyperVision
 
 echo "Done."
--- main.cpp
+++ main.cpp
@@ -1,33 +1,33 @@
 #include <gflags/gflags.h>
 
-
 #include "common.hpp"
 #include "./graph_analyze/detector_main.hpp"
 
-
 using namespace std;
 
+DEFINE_string(config, "../configuration/lrscan/charrdos.json", "Configuration file location.");
 
-DEFINE_string(config, "../configuration/lrscan/http_lrscan.json",  "Configuration file location.");
-
-
-int main(int argc, char * argv[]) {
+int main(int argc, char *argv[])
+{
     __START_FTIMMER__
 
     google::ParseCommandLineFlags(&argc, &argv, true);
 
     json config_j;
-    try {
+    try
+    {
         ifstream fin(FLAGS_config, ios::in);
         fin >> config_j;
-    } catch (const exception & e) {
+    }
+    catch (const exception &e)
+    {
         FATAL_ERROR(e.what());
     }
 
     auto hv1 = make_shared<Hypervision::hypervision_detector>();
     hv1->config_via_json(config_j);
     hv1->start();
- 
+
     __STOP_FTIMER__
     __PRINTF_EXE_TIME__
 
